a551b21c7884d138e837ee110e40847ace215432
Storm Dragon <stormdragon2976@gmail.com>
2012-09-26 23:51:53 -0400

    Fixed an error while linking sounds directory in multicharacter.sh. Everything should work as expected now.

--- multicharacter-setup.sh
+++ multicharacter-setup.sh
@@ -40,7 +40,7 @@ create_character()
         exit 1
     fi
     if [ -d "sounds" ] ; then
-        ln -s $(pwd)/sounds/ $characterPath/sounds/
+        ln -s $(pwd)/sounds/ $characterPath/
      else
         echo "Error! Missing sounds directory. Exiting."
         exit 1
